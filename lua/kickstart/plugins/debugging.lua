70cd4d7f8f28751179e97cce2b970578d30a2bc9
alz59 <22027263+alz59@users.noreply.github.com>
2024-06-04 15:45:41 -0400

    Add Debugging
    Python crashes??

---
+++ lua/kickstart/plugins/debugging.lua
@@ -0,0 +1,82 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'mfussenegger/nvim-dap-python',
+    'nvim-neotest/nvim-nio',
+    'leoluz/nvim-dap-go',
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>B', function()
+      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    end, { desc = 'Debug: Set Breakpoint' })
+    require('dap-python').setup()
+    require('dapui').setup()
+    require('dap-go').setup()
+    dap.adapters.bashdb = {
+      type = 'executable',
+      command = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+      name = 'bashdb',
+    }
+    dap.configurations.sh = {
+      {
+        type = 'bashdb',
+        request = 'launch',
+        name = 'Launch file',
+        showDebugOutput = true,
+        pathBashdb = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+        pathBashdbLib = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+        trace = true,
+        file = '${file}',
+        program = '${file}',
+        cwd = '${workspaceFolder}',
+        pathCat = 'cat',
+        pathBash = '/usr/local/bin/bash',
+        pathMkfifo = 'mkfifo',
+        pathPkill = 'pkill',
+        args = {},
+        env = {},
+        terminalKind = 'integrated',
+      },
+    }
+    -- dap.configurations.python = {
+    --   {
+    --     type = 'python',
+    --     request = 'launch',
+    --     name = 'Launch File',
+    --     program = '${file}',
+    --     pythonPath = function()
+    --       return '/usr/local/bin/python3'
+    --     end,
+    --   },
+    -- }
+    -- dap.adapters.python = {
+    --   type = 'executable',
+    --   command = os.getenv '~/.virtualenvs/debugpy/bin/python',
+    --   args = { '-m', 'debugpy.adapter' },
+    -- }
+    -- local dap, dapui = require 'dap', require 'dapui'
+    -- Basic debugging keymaps, feel free to change to your liking!
+  end,
+}
